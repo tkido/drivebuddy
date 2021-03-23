6623c26d5cde0215bf7befefb044bdaf14384b58
tkido <tkido@xevo.com>
2021-03-23 14:50:17 +0900

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,21 @@ module github.com/tkido/drivebuddy
 
 go 1.13
 
-require github.com/magefile/mage v1.11.0
+require (
+	github.com/ChimeraCoder/anaconda v2.0.0+incompatible
+	github.com/ChimeraCoder/tokenbucket v0.0.0-20131201223612-c5a927568de7 // indirect
+	github.com/aws/aws-sdk-go v1.37.29 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/polly v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.2.0 // indirect
+	github.com/azr/backoff v0.0.0-20160115115103-53511d3c7330 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/dghubble/oauth1 v0.6.0
+	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
+	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/magefile/mage v1.11.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/polly"
+
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello drive buddy!!")
+
+	// The name of the text file to convert to MP3
+	fileName := `_testdata/glass.txt`
+
+	// Open text file and get it's contents as a string
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Got error opening file " + fileName)
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// Convert bytes to string
+	s := string(contents[:])
+
+	// Initialize a session that the SDK uses to load
+	// credentials from the shared credentials file. (~/.aws/credentials).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create Polly client
+	svc := polly.New(sess)
+
+	// Output to MP3 using voice Joanna
+	input := &polly.SynthesizeSpeechInput{OutputFormat: aws.String("mp3"), Text: aws.String(s), VoiceId: aws.String("Joanna")}
+
+	output, err := svc.SynthesizeSpeech(input)
+	if err != nil {
+		fmt.Println("Got error calling SynthesizeSpeech:")
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// Save as MP3
+	names := strings.Split(fileName, ".")
+	name := names[0]
+	mp3File := name + ".mp3"
+
+	outFile, err := os.Create(mp3File)
+	if err != nil {
+		fmt.Println("Got error creating " + mp3File + ":")
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	defer outFile.Close()
+	_, err = io.Copy(outFile, output.AudioStream)
+	if err != nil {
+		fmt.Println("Got error saving MP3:")
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 }
